7f02e0314890e679a60ebd8d38cd81df7496ae48
Yoiner <yoinerpertuz@gmail.com>
2025-10-01 17:16:09 -0500

    firebase auth configured with .env variables

--- src/core/firebase.ts
+++ src/core/firebase.ts
@@ -1,9 +1,13 @@
 import admin from "firebase-admin";
-import serviceAccount from "../../google-services.json" with { type: "json" };
+import "dotenv/config";
 
 if (!admin.apps.length) { // ✅ evita inicializar más de una vez
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID || "",
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL || "",
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || ""
+    }),
   });
 }
 
